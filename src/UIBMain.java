6a4b465928b9776b6c60393cebed7e15704fef49
Renato de Sá Loreto <1192306616@acd.grupotiradentes.com>
2020-03-10 21:52:00 -0300

    terceiro commit

--- src/UIBMain.java
+++ src/UIBMain.java
@@ -2,18 +2,18 @@
 public class UIBMain {
 
 	public static void main(String[] args) {
-		
+
 		Cliente cliente = new Cliente();
 		cliente.nome = "Seu jair";
 		cliente.cpf = "123";
-		
+
 		Conta conta = new Conta();
 		conta.numero = "123-1";
 		conta.saldo = 100;
 		conta.cliente = cliente;
-		
+
 		cliente.cpf = "987";
-		
+
 		System.out.println(conta.cliente.nome);
 		System.out.println(conta.cliente.cpf);
 	}
